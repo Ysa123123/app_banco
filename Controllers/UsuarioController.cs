86e2ec20f805f17876b2194192f70954c14d0947
Ysa123123 <ysabella.cristina2008@gmail.com>
2026-05-29 15:19:19 -0300

    Metodo Atuslizar + Excluir

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Segundo_App_BancoDados.Models;
+using Segundo_App_BancoDados.Repository.Contract;
+
+namespace Segundo_App_BancoDados.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        private IUsuarioRepository _usuarioRepository;
+
+        public UsuarioController(IUsuarioRepository usuarioRepository)
+        {
+            _usuarioRepository = usuarioRepository;
+        }
+        public IActionResult Index()
+        {
+            return View(_usuarioRepository.ObterTodosUsuarios());
+        }
+        [HttpGet]
+        public IActionResult CadastrarUsuario()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CadastrarUsuario(Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                _usuarioRepository.Cadastrar(usuario);
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Atualizar(int id)
+        {
+            return View(_usuarioRepository.ObterUsuario(id));
+        }
+        [HttpPost]
+        public IActionResult Atualizar(Usuario usuario)
+        {
+            return View();
+        }
+
+}
---
+++ Repository/UsuarioRepository.cs
@@ -0,0 +1,120 @@
+﻿using MySql.Data.MySqlClient;
+using Segundo_App_BancoDados.Models;
+using Segundo_App_BancoDados.Repository.Contract;
+using System.Data;
+
+namespace Segundo_App_BancoDados.Repository
+{
+    public class UsuarioRepository : IUsuarioRepository
+    {
+        private readonly string _conexaoMySQL;
+
+        public UsuarioRepository(IConfiguration conf)
+        {
+           _conexaoMySQL = conf.GetConnectionString("ConexaoMySQL");
+        }
+        public void Atualizar(Usuario usuario)
+        {
+            using (var conexao = new MySqlConnection(_conexaoMySQL))
+            {
+                conexao.Open();
+                MySqlCommand cmd = new MySqlCommand("Update usuario set nomeUsu=@nomeUsu, Cargo=@Cargo, " +
+                                                    " DataNasc=@DataNasc Where IdUsu=@IdUsu;", conexao);
+
+                cmd.Parameters.Add("@nomeUsu", MySqlDbType.VarChar).Value = usuario.nomeUsu;
+                cmd.Parameters.Add("@Cargo", MySqlDbType.VarChar).Value = usuario.Cargo;
+                cmd.Parameters.Add("@DataNasc", MySqlDbType.VarChar).Value = usuario.DataNasc.ToString("yyyy/MM/dd");
+                cmd.Parameters.Add("@IdUsu", MySqlDbType.VarChar).Value = usuario.IdUsu;
+
+                cmd.ExecuteNonQuery();
+                conexao.Close();
+            }
+        }
+
+        public void Cadastrar(Usuario usuario)
+        {
+            using (var conexao = new MySqlConnection(_conexaoMySQL))
+            {
+                conexao.Open();
+
+                MySqlCommand cmd = new MySqlCommand("insert into usuario(nomeUsu, Cargo, DataNasc) " +
+                                                " values (@nomeUsu, @Cargo, @DataNasc )", conexao); // @: PARAMETRO
+
+                cmd.Parameters.Add("@nomeUsu", MySqlDbType.VarChar).Value = usuario.nomeUsu;
+                cmd.Parameters.Add("@Cargo", MySqlDbType.VarChar).Value = usuario.cargo;
+                // cmd.Parameters.Add("@DataNasc", MySqlDbType.VarChar).Value = usuario.DataNasc.ToString("yyyy/MM/dd");
+                cmd.Parameters.Add("@DataNasc", MySqlDbType.VarChar).Value = usuario.dataNasc.ToString("yyyy/MM/dd");
+
+                cmd.ExecuteNonQuery();
+                conexao.Close();
+            }
+        }
+
+        public void Excluir(int id)
+        {
+            using (var conexao = new MySqlConnection(_conexaoMySQL))
+            {
+                conexao.Open();
+                MySqlCommand cmd = new MySqlCommand("delete from usuario where IdUsu=@IdUsu", conexao);
+                cmd.Parameters.AddWithValue("@IdUsu", Id);
+                int i = cmd.ExecuteNonQuery();
+                conexao.Close();
+            }
+        }
+
+        public IEnumerable<Usuario> ObterTodosUsuarios()
+        {
+            List<Usuario> UsuarioList = new List<Usuario>();
+            using (var conexao = new MySqlConnection(_conexaoMySQL))
+            {
+                conexao.Open();
+                MySqlCommand cmd = new MySqlCommand("Select * from usuario", conexao);
+
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                conexao.Clone();
+
+                foreach (DataRow dr in dt.Rows)
+                {
+                    UsuarioList.Add(
+                        new Usuario
+                        {
+                            IdUsu = Convert.ToInt32(dr["IdUsu"]),
+                            nomeUsu = (string)dr["nomeUsu"],
+                            cargo = (string)dr["Cargo"],
+                            dataNasc = Convert.ToDateTime(dr["DataNasc"])
+
+                        });
+                }
+                return UsuarioList;
+            }
+        }
+
+        public Usuario ObterUsuario(int id)
+        {
+            using (var conexao = new MySqlConnection(_conexaoMySQL))
+            {
+                conexao.Open();
+                MySqlCommand cmd = new MySqlCommand("SELECT * from usuario " +
+                                                    " where IdUsu=@IdUsu", conexao);
+                cmd.Parameters.AddWithValue("@IdUsu", Id);
+
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                MySqlDataReader dr;
+
+                Usuario usuario = new Usuario();
+                dr = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+                while (dr.Read())
+                {
+                    usuario.IdUsu = Convert.ToInt32(dr["IdUsu"]);
+                    usuario.nomeUsu = (string)(dr["nomeUsu"]);
+                    usuario.Cargo = (string)(dr["Cargo"]);
+                    usuario.DataNasc = Convert.ToDateTime(dr["DataNasc"]);
+                }
+                return usuario;
+            }
+        }
+    }
+}
